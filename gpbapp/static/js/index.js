7e74ab943790fb3b447a762ff3af39b7e107dc89
Yohan V <vienne.yohan@gmail.com>
2018-04-02 20:23:09 +0200

    Add Wiki Media function

--- gpbapp/static/js/index.js
+++ gpbapp/static/js/index.js
@@ -1,29 +1,33 @@
 var form = document.querySelector("form");
 var map = document.querySelector("#map");
 
+////////////////////////////
+// Google Map API function//
+////////////////////////////
+
 // Map Initialize
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
-        center: {lat: -34.397, lng: 150.644}
+        center: { lat: 34.2331373, lng: -102.4107493}
     });
     var geocoder = new google.maps.Geocoder();
 
     document.getElementById('submit').addEventListener('click', function() {
         var userQuestion = document.getElementById('userQuestion').value;
-        requestQuestion(geocoder, map, userQuestion);
+        requestQuestionMap(geocoder, map, userQuestion);
     });
 }
 
 // Request the user question to the API
-function requestQuestion(geocoder, map, userQuestion) {
+function requestQuestionMap(geocoder, map, userQuestion) {
     var reqQuestion = new XMLHttpRequest();
     var encodeVar = encodeURIComponent(userQuestion);
     reqQuestion.open("GET", "http://localhost:5000/question/" + encodeVar);
     reqQuestion.onreadystatechange = function () {
         if (reqQuestion.readyState == 4 && (reqQuestion.status >= 200 || reqQuestion.status == 0)) {
             var readData = reqQuestion.responseText;
-            console.log("readData: " + readData);
+            console.log("readDataMap: " + readData);
             geocodeAddress(geocoder, map, readData);
         }
     };
@@ -37,10 +41,79 @@ function geocodeAddress(geocoder, resultsMap, address) {
             resultsMap.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
             map: resultsMap,
-            position: results[0].geometry.location
-        });
+            position: results[0].geometry.location,
+            });
         } else {
             alert("Je n'ai pas compris ta question ");
         }
     });
+}
+
+/////////////////////////
+// Wiki Media function //
+/////////////////////////
+
+document.getElementById('submit').addEventListener('click', function () {
+    var userQuestion = document.getElementById('userQuestion').value;
+    requestQuestionWiki(userQuestion);
+});
+
+//Request the user_question to API
+function requestQuestionWiki(userQuestion) {
+    var reqQuestion = new XMLHttpRequest();
+    var encodeVar = encodeURIComponent(userQuestion);
+    reqQuestion.open("GET", "http://localhost:5000/wiki/" + encodeVar, true);
+    reqQuestion.onreadystatechange = function () {
+        if (reqQuestion.readyState == 4 && (reqQuestion.status >= 200 || reqQuestion.status == 0)) {
+            var readData = JSON.parse(reqQuestion.responseText);
+            dataOutput = readData.lat + "|" + readData.lng;
+            console.log("readDataWiki: " + dataOutput);
+            requestWiki(dataOutput);     
+        }
+    };
+    reqQuestion.send(null);
+}
+
+//get the coordinate and search in Wiki Media geosearch function
+function requestWiki(geo) {
+    var reqWiki = new XMLHttpRequest();
+    var url = "https://fr.wikipedia.org/w/api.php?action=query&format=json&uselang=fr&list=geosearch&gscoord=" + geo;
+    reqWiki.open("GET", url, true);
+    reqWiki.onreadystatechange = function () {
+        if (reqWiki.readyState == 4 && (reqWiki.status >= 200 || reqWiki.status == 0)) {
+            var readData = JSON.parse(reqWiki.responseText);
+            var readDataCount = Object.keys(readData.query.geosearch).length;
+            console.log("readDataCount: " + readDataCount);
+            story(readDataCount, readData);
+        }
+    };    
+    reqWiki.send(null);
+}
+
+// Get a random story about the geosearch
+function story(readDataCount, readData) {
+    var randomNumber = Math.floor(Math.random() * readDataCount);
+    var pageid = readData.query.geosearch[randomNumber].pageid;
+    console.log("random: " + randomNumber);
+    var reqStory = new XMLHttpRequest();
+    var url = "https://fr.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&utf8=1&formatversion=latest&exsentences=3&explaintext=1&exsectionformat=wiki&pageids=" + pageid;
+    reqStory.open("GET", url, true);
+    reqStory.onreadystatechange = function () {
+        if (reqStory.readyState == 4 && (reqStory.status >= 200 || reqStory.status == 0)) {
+            var result = JSON.parse(reqStory.responseText);
+            console.log(result.query.pages[0].extract);
+            var extractStory = result.query.pages[0].extract;
+            var title = result.query.pages[0].title;
+            writeStory(extractStory, title);
+        }
+    };
+    reqStory.send(null);
+}
+
+//Write a random story on HTML page
+function writeStory(extractStory, title) {
+    document.querySelector(".text").innerHTML = extractStory;
+    var link = document.querySelector(".linkWiki");
+    link.setAttribute('href', 'https://fr.wikipedia.org/wiki/' + title);
+    
 }
--- gpbapp/views.py
+++ gpbapp/views.py
@@ -2,6 +2,7 @@
 import json
 from flask import Flask, render_template, request
 from .utils.parserkiller import get_address
+from .utils.googleapi import geocode_request
 app = Flask(__name__)
 
 # Add config.py file
@@ -18,8 +19,15 @@ def index():
 def question(sentence):
     """Get the user question, parse it and show the address on map"""
     parserQuestion = get_address(sentence)
-    print(parserQuestion)
     return parserQuestion
+
+@app.route('/wiki/<sentence>')
+def wiki(sentence):
+    geo = {}
+    parserQuestion = get_address(sentence)
+    geo["lat"], geo["lng"] = geocode_request(parserQuestion)
+    print(geo)
+    return json.dumps(geo)
 
 #if __name__ == "__main__":
 #    app.run()
